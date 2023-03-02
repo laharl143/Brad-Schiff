2fce407a16cb8598dd47574a35a9ba82eb052d5c
Erskine <erskine.duenas@gmail.com>
2023-03-02 15:06:06 +0800

    update

---
+++ notes.js
@@ -0,0 +1,12 @@
+/*
+
+used commands during the tutorial
+
+$ npm init -y
+//The command "npm init -y" initializes a new Node.js project with default settings, without prompting the user to answer any questions.
+
+The "-y" flag stands for "yes," which means that it will automatically answer "yes" to all the prompts that would otherwise require user input. This is a convenient way to quickly create a new project with default settings without having to manually enter them.
+
+Once executed, the command creates a package.json file in the current directory, which is used to manage the project's dependencies, scripts, and metadata.
+
+*/
